6238556f2643a7bb6ff624c2b6e1d544d74d0cc0
Jian Liao <jian.liao@gmail.com>
2022-12-14 09:52:10 -0800

    Upgrade to latest @swc-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import "./App.css";
 
+import React from "react";
 import { Theme } from "@swc-react/theme";
 import { Button } from "@swc-react/button";
 import { Card } from "@swc-react/card";
 import { Radio, RadioGroup } from "@swc-react/radio";
-import { ActionGroup } from "@swc-react/action-group";
-import { ActionButton } from "@swc-react/action-button";
+import { ActionGroup, ActionGroupType } from "@swc-react/action-group";
+import { ActionButton, ActionButtonType } from "@swc-react/action-button";
 import { ProgressCircle } from "@swc-react/progress-circle";
 import { IconEdit } from "@swc-react/icons-workflow/Edit";
 
+import '@spectrum-web-components/theme/theme-light.js';
+import '@spectrum-web-components/theme/scale-medium.js';
+
 function App() {
+  const handleChange = (e: Event) => {
+    console.log((e.target as ActionGroupType).selected);
+  };
+  const handleClick = (e: React.MouseEvent<ActionButtonType>) => {
+    console.log(e.currentTarget.selected);
+  }
   return (
     <Theme theme="spectrum" scale="medium" color="light">
       <div className="App">
-        <Button click={() => alert("Hello")}>
+        <Button onClick={() => alert("Hello")}>
           <IconEdit slot="icon"></IconEdit>Be right back
         </Button>
         <Card heading="Card Heading" subheading="JPG Photo">
@@ -30,12 +40,13 @@ function App() {
           <Radio value="third">Option 3</Radio>
           <Radio value="fourth">Option 4</Radio>
         </RadioGroup>
-        <ActionGroup selects="single" selected={["third"]}>
+        <ActionGroup selects="single" selected={["third"]} change={handleChange}>
           <ActionButton value="first">First</ActionButton>
           <ActionButton value="second">Second</ActionButton>
           <ActionButton value="third">Third</ActionButton>
         </ActionGroup>
         <ProgressCircle size="xxl" indeterminate />
+        <ActionButton toggles change={handleChange} onClick={handleClick}>Toggle</ActionButton>
       </div>
     </Theme>
   );
